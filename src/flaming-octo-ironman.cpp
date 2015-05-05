466b4ef3a700ff368f4a2b29a6b26c8770e8a00a
newsworthy39 <michaeljensendk@hotmail.com>
2015-05-05 16:23:50 +0200

    Fun and games, literally.

--- src/flaming-octo-ironman.cpp
+++ src/flaming-octo-ironman.cpp
@@ -8,17 +8,30 @@
 
 using namespace std;
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+int main() {
+
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "SFML works!",
+            sf::Style::Default, settings);
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                // left key is pressed: move our character
+                shape.move(1, 0);
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                // left key is pressed: move our character
+                shape.move(-1, 0);
+            }
+
             if (event.type == sf::Event::Closed)
                 window.close();
         }
