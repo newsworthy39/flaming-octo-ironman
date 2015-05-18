91519b600ae42ba878aaf4c7b3ab82f262d50b65
newsworthy39 <michaeljensendk@hotmail.com>
2015-05-18 20:10:34 +0200

    New release build

--- Release/src/objects/subdir.mk
+++ Release/src/objects/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 src/objects/%.o: ../src/objects/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I"/home/gandalf/workspace/flaming-octo-ironman/src" -O2 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -std=c++11 -I"/home/gandalf/workspace/flaming-octo-ironman/src" -O2 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
