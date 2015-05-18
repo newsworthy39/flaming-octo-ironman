fbec1e3e4f779c9e2a8a575d1823fc01be1c7787
newsworthy39 <michaeljensendk@hotmail.com>
2015-05-18 20:21:05 +0200

    Added missing build-files

---
+++ Debug/src/scenes/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/scenes/Dashboard.cpp 
+
+OBJS += \
+./src/scenes/Dashboard.o 
+
+CPP_DEPS += \
+./src/scenes/Dashboard.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/scenes/%.o: ../src/scenes/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -std=c++11 -D__DEBUG__ -I"/home/gandalf/workspace/flaming-octo-ironman/libs/SFML-2.2/include" -I"/home/gandalf/workspace/flaming-octo-ironman/src" -O2 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
